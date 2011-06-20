fb7e596e967059bbe19d1939cb6399d7e4ad91f4
Jan Scholz <scholz@imf.au.dk>
2011-06-21 01:06:36 +0200

    automatic setting of spinup etc parameters

--- CFSRwrapper.py
+++ CFSRwrapper.py
@@ -1,16 +1,37 @@
 import pygrib
+import re
 
 class CFSRwrapper(object):
     """Iterator for traversing grbs file skipping spin-up timestamps"""
     recpertimestep=None
     spinup=None
     nonspinup=None
+    instant=None
     grbs=None
-    def __init__(self, fname,recpertimestep,spinup=1,nonspinup=6):
+    fieldnametoparam={
+        'dswsfc':(1,1,6,False),
+        'tmp2m':(1,1,6,True),
+        'wnd10m':(2,1,6,True),
+        }
+    def __init__(self, fname,recpertimestep=None,spinup=None,nonspinup=None,instant=None,autoconf=True):
+        if autoconf:
+            assert(recpertimestep==None)
+            assert(spinup==None)
+            assert(nonspinup==None)
+            assert(instant==None)
+            m=re.match("([^\./]+)\.l\.gdas\..*grb2",fname)
+            if m:
+                fieldname=m.groups()[0]
+            else:
+                fieldname=re.match("([^\./]+)\.gdas\..*grb2",fname).groups()[0]
+            (self.recpertimestep,self.spinup,self.nonspinup,self.instant)=self.fieldnametoparam[fieldname]
+        else:
+            self.recpertimestep=recpertimestep
+            self.spinup=spinup
+            self.nonspinup=nonspinup
+            self.instant=instant
         self.grbs=pygrib.open(fname)
-        self.recpertimestep=recpertimestep
-        self.spinup=spinup
-        self.nonspinup=nonspinup
+                
     def __iter__(self):
         self.grbs.rewind()
         return self
