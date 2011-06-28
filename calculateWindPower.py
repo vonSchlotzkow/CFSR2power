e6e3b179bf5dee40ec00839000dace22f8c77500
Jan Scholz <scholz@imf.au.dk>
2011-06-28 18:02:08 +0200

    add sample application to convert wind and radiation to generated power

---
+++ calculateWindPower.py
@@ -0,0 +1,31 @@
+from CFSRwrapper import *
+
+def openfields(infields,year,month,lowres=False):
+    import itertools
+    r=[]
+    for f in infields:
+        r.append(CFSRwrapper(filenamefromfield(f,2000,3,lowres)))
+    return itertools.izip(*r)
+
+def unpackandapply(i,conv):
+    return conv(map(lambda x:x[0].data,i))
+
+def iterateandapply(it,conv):
+    for i in it:
+        c=unpackandapply(i,conv)
+
+def PWconversion(i):
+    """Plain Wrong conversion of windspeed and radiation to power"""
+    return i[0]**3 + i[1]
+
+infields=['wnd10m','dswsfc']
+
+it=openfields(infields,2000,3,True)
+
+#convert just one timestep
+i=it.next()
+unpackandapply(i,PWconv)
+
+#convert all
+iterateandapply(i,PWconv)
+
