0eff9405549e40884a802c5f778b69734a4bdfef
Jan Scholz <scholz@imf.au.dk>
2011-06-30 19:07:40 +0200

    saving converted values to grib file (rudimentary)

--- calculateWindPower.py
+++ calculateWindPower.py
@@ -7,12 +7,17 @@ def openfields(infields,year,month,lowres=False):
         r.append(CFSRwrapper(filenamefromfield(f,2000,3,lowres)))
     return itertools.izip(*r)
 
-def unpackandapply(i,conv):
-    return conv(map(lambda x:x[0].data,i))
+def unpackandapply(i,conv,outf):
+    rnp=conv(map(lambda x:x[0].data,i))
+    rmc=i[0][0]
+    rmc.putdata(rnp)
+    outf.write(rmc.grbmsg.tostring())
+    return rmc
 
-def iterateandapply(it,conv):
+def iterateandapply(it,conv,outf):
+    assert(outf.mode=='wb')
     for i in it:
-        c=unpackandapply(i,conv)
+        rmc=unpackandapply(i,conv,outf)
 
 def PWconversion(i):
     """Plain Wrong conversion of windspeed and radiation to power"""
@@ -22,10 +27,13 @@ infields=['wnd10m','dswsfc']
 
 it=openfields(infields,2000,3,True)
 
-#convert just one timestep
-i=it.next()
-unpackandapply(i,PWconversion)
+outf=file("PWconversion.output.grib",'wb')
 
-#convert all
-iterateandapply(it,PWconversion)
+# #convert just one timestep
+#i=it.next()
+#unpackandapply(i,PWconversion,outf)
+
+# #convert all
+#iterateandapply(it,PWconversion,outf)
+
 
