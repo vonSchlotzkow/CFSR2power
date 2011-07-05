91710462063531b0f9af658e86ec8e974fff2e1c
Jan Scholz <scholz@imf.au.dk>
2011-07-06 00:49:49 +0200

    [example] demonstrate local options

--- calculateWindPower.py
+++ calculateWindPower.py
@@ -1,5 +1,6 @@
 from CFSRwrapper import *
 from optparse import OptionParser
+from optparse import OptionGroup
 
 parser = OptionParser()
 parser.add_option("--year", dest="year", default=2000, type=int,
@@ -13,11 +14,17 @@ parser.add_option("--debug",
                   action="store_true", dest="debug",
                   help="run in debug mode")
 
+locoptions=OptionGroup(parser, "PWconversion options", "Options related to PWconversion")
+locoptions.add_option("--nonsensfactor", dest="nonsensfactor", default=1, type=float,
+                      help="stupid factor of the plain wrong conversion")
+
+parser.add_option_group(locoptions)
+
 (options, args) = parser.parse_args()
 
-def PWconversion(i):
+def PWconversion(i,nonsensfactor):
     """Plain Wrong conversion of windspeed and radiation to power"""
-    return i[0]**3 + i[1]
+    return i[0]**3 + nonsensfactor*i[1]
 
 infields=['wnd10m','dswsfc']
 
@@ -25,10 +32,12 @@ it=openfields(infields,options.year,options.month,options.lowres)
 
 outf=file(filenamefromfield("PWpower",options.year,options.month,options.lowres),'wb')
 
+convfunc=lambda x:PWconversion(x,options.nonsensfactor)
+
 if options.debug:
     #convert just one timestep
     i=it.next()
-    unpackandapply(i,PWconversion,outf)
+    unpackandapply(i,convfunc,outf)
 else:
     #convert all
-    iterateandapply(it,PWconversion,outf)
+    iterateandapply(it,convfunc,outf)
