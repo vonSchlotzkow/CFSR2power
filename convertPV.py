b85436bbdd859e6cf7f53fd847ad9adf74e22220
Jan Scholz <scholz@imf.au.dk>
2011-07-06 14:38:20 +0200

    [calcPV] first shot at really converting temperature and radiation to generated PV power

--- convertPV.py
+++ convertPV.py
@@ -1,26 +1,45 @@
 #!/usr/bin/env python
 from CFSRwrapper import *
 from optparse import OptionGroup
+from numpy import zeros,log
 
-locoptions=OptionGroup(parser, "PWconversion options", "Options related to PWconversion")
-locoptions.add_option("--nonsensfactor", dest="nonsensfactor", default=1, type=float,
-                      help="stupid factor of the plain wrong conversion")
+locoptions=OptionGroup(parser, "PV conversion options", "Options related to conversion of radiation and temperature to power")
+locoptions.add_option("--solarpaneltype", dest="solarpaneltypefile", default=None, type=str,
+                      help="file containing characteristics of the solar panels")
 
 parser.add_option_group(locoptions)
 
 (options, args) = parser.parse_args()
 
-def PWconversion(i,nonsensfactor):
-    """Plain Wrong conversion of windspeed and radiation to power"""
-    return i[0]**3 + nonsensfactor*i[1]
+infields=['dswsfc','tmp2m']
 
-infields=['wnd10m','dswsfc']
+def SolarPVConversion(data):
+    """Conversion of radiation and temperature to solar PV power"""
+    I=data[0]  #Radiation
+    T=data[1]  #Temperature
+
+    #Solar panel data
+    threshold=173.43
+    A=-0.624
+    B=-0.0000883
+    C=0.124
+    D=0.0048
+    NOCT=50.
+
+    #Mask for lower threshold
+    index = I>threshold
+
+    #Convert to solar PV power
+    P = zeros(I.shape)
+    P[index]=I[index]*(A+(B*I[index])+(C*log(I[index])))*(1.0-D*(T[index]-298.0+((NOCT-20.)/800.*I[index])))
+    P[I<=threshold]=0.
+    return P
 
 it=openfields(infields,options.year,options.month,options.lowres)
 
-outf=file(filenamefromfield("PWpower",options.year,options.month,options.lowres),'wb')
+outf=file(filenamefromfield("PVpower",options.year,options.month,options.lowres),'wb')
 
-convfunc=lambda x:PWconversion(x,options.nonsensfactor)
+convfunc=SolarPVConversion
 
 if options.debug:
     #convert just one timestep
