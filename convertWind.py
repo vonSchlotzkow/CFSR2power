d9ce86ec68468026e658d7c2f90a3686cab27208
Jan Scholz <scholz@imf.au.dk>
2011-07-07 12:31:41 +0200

    [calcWind] allow specification of different turbine types through config files

--- convertWind.py
+++ convertWind.py
@@ -2,6 +2,9 @@
 from CFSRwrapper import *
 from optparse import OptionGroup
 from pylab import interp
+from configobj import ConfigObj
+from validate import Validator
+from StringIO import StringIO
 
 locoptions=OptionGroup(parser, "Wind conversion options", "Options related to converting ")
 locoptions.add_option("--turbinecurve", dest="turbinecurve", default=None, type=str,
@@ -11,18 +14,46 @@ parser.add_option_group(locoptions)
 
 (options, args) = parser.parse_args()
 
-def PWconversion(i,nonsensfactor):
-    """Plain Wrong conversion of windspeed and radiation to power"""
-    return i[0]**3
+infields=['wnd10m']
 
-def WindConversion(data):
+#the config file spec
+spec=StringIO("""
+# Turbine characteristic data
+name = string
+# The manufacturer of this turbine
+manufacturer = string
+# Link to the original datasheet of the turbine
+source = string
+
+# Hub height
+H = float
+# Power curve, specified as lists of velocities and powers, including
+# cut-in and cut-out speeds
+# Power curve velocities
+V = list
+# Generated power from the power curves
+POW = list
+""")
+configspec=ConfigObj(spec,
+                     list_values=False, file_error=True, _inspec=True)
+turbineconfig=ConfigObj(options.turbinecurve,
+                        list_values=True, file_error=True,
+                        configspec=configspec)
+turbineconfig.validate(Validator())
+# it would be more elegant to give the requirement for lists of floats
+# in the spec, which should result in a conversion to floats at the
+# same time, but well...
+for listkey in ['V','POW']:
+    turbineconfig[listkey]=map(float,turbineconfig[listkey])
+
+def WindConversion(data,c):
     """Conversion of wind speed to wind power"""
     wind10m = data[0]
     
     #Turbine data
-    H = 80. #Hub height
-    V = [0,3,4,5,6,7,8,9,10,11,12,12.5,23.0,23.0] #Power curve velocities
-    POW = [0.000,0.000,.110,.230,.405,.720,1.080,1.480,1.800,2.100,2.370,2.600,2.600,0.000] #power from the power curves
+    H = c['H'] #Hub height
+    V = c['V'] #Power curve velocities
+    POW = c['POW'] #power from the power curves
    
     #Convert wind speed to hub height H from a height 10m above the ground
     #0.143 is power law index which depends on roughness of the surface and assumed to be constant for the time being.
@@ -33,15 +64,12 @@ def WindConversion(data):
 
     return P
 
-infields=['wnd10m']
-
 it=openfields(infields,options.year,options.month,options.lowres)
 
-outf=file(filenamefromfield("WindPower",options.year,options.month,options.lowres),'wb')
+outf=file(filenamefromfield("WindPower_%s" % turbineconfig['name'],options.year,options.month,options.lowres),'wb')
 
 #example of binding additional options to the conversion function
-#convfunc=lambda x:PWconversion(x,options.nonsensfactor)
-convfunc=WindConversion
+convfunc=lambda x:WindConversion(x,turbineconfig)
 
 if options.debug:
     #convert just one timestep
